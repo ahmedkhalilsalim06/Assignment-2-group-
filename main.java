19e711321b0968b6993660df6b67a02e2207a7b5
khalil <sakhalils06@gmail.com>
2026-01-28 11:57:09 +0300

    fix

--- main.java
+++ main.java
@@ -1,19 +1,8 @@
 import java.util.Random;
 import java.util.random.*;
 import java.util.Arrays;
-public class main {
-    public static void main(String[] args) {
-        //System.out.println(Arrays.toString(array_maker(10)));
-        
-    } public static int[] array_maker(int size){
-        Random random = new Random();
-        int[] arr = new int[size];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(101);
-        }
-        return arr;
-    }
-}
+
+    
 
 import java.util.Scanner;
 class main{
@@ -115,5 +104,12 @@ class main{
             }
         }
        return "The even sum: " + sumE + "\n" + "The odd sum: " + sumO;
+    } public static int[] array_maker(int size){
+        Random random = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(101);
+        }
+        return arr;
     }
 }
