e2075ab477459040bd3181d531b7aa42adacac80
Boyukagha Aghayev <aghayev.boyuk@gmail.com>
2026-01-28 11:44:45 +0300

    Complete MaxMinAv

--- main.java
+++ main.java
@@ -1,39 +1,4 @@
-public class main {
 
-    public static int findMinimum(int[] Array) {
-        int minimum = 101; // Define minimum with default value
-
-        // Iterate through the entire array and find the minimum element
-        for (int i = 0; i < Array.length; i++) {
-            // Get current element
-            int currentElement = Array[i]; 
-
-            // if current element is less than minimum, set minimum to currentElement
-            if ( currentElement < minimum) {
-                minimum = currentElement;
-            }
-        }
-
-        return minimum;
-    }
-
-    public static int findMaximum(int[] Array) {
-        int maximum = 0; // Define minimum with default value
-
-        // Iterate through the entire array and find the minimum element
-        for (int i = 0; i < Array.length; i++) {
-            // Get current element
-            int currentElement = Array[i];
-
-            // if current element is less than minimum, set minimum to currentElement
-            if ( currentElement < maximum) {
-                maximum = currentElement;
-            }
-        }
-
-        return maximum;
-    }
-}
 import java.util.Scanner;
 class main{
     public static void main(String [] args){
@@ -89,5 +54,37 @@ class main{
         }
         return arr;
     }
+    public static int findMinimum(int[] Array) {
+        int minimum = 101; // Define minimum with default value
 
+        // Iterate through the entire array and find the minimum element
+        for (int i = 0; i < Array.length; i++) {
+            // Get current element
+            int currentElement = Array[i]; 
+
+            // if current element is less than minimum, set minimum to currentElement
+            if ( currentElement < minimum) {
+                minimum = currentElement;
+            }
+        }
+
+        return minimum;
+    }
+
+    public static int findMaximum(int[] Array) {
+        int maximum = 0; // Define minimum with default value
+
+        // Iterate through the entire array and find the minimum element
+        for (int i = 0; i < Array.length; i++) {
+            // Get current element
+            int currentElement = Array[i];
+
+            // if current element is less than minimum, set minimum to currentElement
+            if ( currentElement < maximum) {
+                maximum = currentElement;
+            }
+        }
+
+        return maximum;
+    }
 }
