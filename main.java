1542675df19d990f25eb493f6a9d0e72cd7b6005
Ogtay Talishkhanli <oktay2008t@gmail.com>
2026-01-28 11:23:14 +0300

    interface1

---
+++ main.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+class main{
+    public static void main(String [] args){
+
+
+        Scanner sc = new Scanner(System.in);
+        boolean cont = true;
+
+        System.out.println("--------Salam--------");
+        String command;
+        
+
+        while (cont){
+            System.out.println("1. Find the max and the min");
+            System.out.println("2. Find the average and  how each element of the array differs from the average");
+            System.out.println("3. Find the sum of elements with odd- and even-numbered indexes. ");
+            System.out.println("4. End");
+            System.out.println("Choose an operation:");
+
+        command = sc.nextLine();
+
+        switch (command){
+            case "1":
+
+            break;
+            case "2":
+
+            break;
+            case "3":
+
+            break;
+            case "4":
+                cont = false;
+            break;
+            default:
+                System.out.println("Choose a valid operation!");
+        }
+
+        }
+
+
+        System.out.println("End!");
+    }
+
+
+
+}
